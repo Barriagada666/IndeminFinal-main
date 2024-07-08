be6daef000b02745a4d811c366dee29066d7ffe3
BORIS HECTOR ARRIAGADA <bori.arriagada@duocuc.cl>
2024-07-07 21:53:39 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,11 @@
+...# IndeminFinal
+
+ se debe instalar nodejs e ionic
+ 
+proyecto basado en IONIC 7 
+
+SE DEBE USAR COMANDO :
+
+npm install --force
+
+ionic serve 
---
+++ indemin-final/src/app/services/usuario.service.ts
@@ -0,0 +1,18 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { IUserLogin } from '../models/IUserLogin';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class UsuarioService {
+
+  private baseUrl = 'https://backend-indemin.onrender.com'; // URL de tu backend
+
+  constructor(private http: HttpClient) { }
+
+  authenticateUser(userLogin: IUserLogin): Observable<any> {
+    return this.http.get<any>(`${this.baseUrl}/authenticate?email=${userLogin.email}&password=${userLogin.password}`);
+  }
+}
